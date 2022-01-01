043a68c618b204818644a48a4966feabb605026f
Deboraanaiv <92692128+Deboraanaiv@users.noreply.github.com>
2022-01-01 19:19:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DIO.series
+{
+   class program
+
+        static void Main(string[] args)
+    {
+        entidadebase.class MinhaClasse = new entidadebase.class ();
+        Serie meuObjeto = new Serie ();
+    }
